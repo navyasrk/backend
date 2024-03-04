845f0cd8642d341bbe90f34cd516b4a029592ed6
navyasrk <k.navyasree99@gmail.com>
2024-03-04 10:58:16 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';  
+import mongoose from 'mongoose';
+
+import router from './router';
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+app.use(cors({
+  credentials: true,
+}));
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log('Server running on http://localhost:5501');
+});
+
+
+const MONGO_URL = 'mongodb+srv://shinchan:nightingale@cluster0.y0gzl8x.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
